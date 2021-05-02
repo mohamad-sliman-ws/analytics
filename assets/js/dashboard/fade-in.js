402947bf7e7062eaae9738b1a7ce4c4572107c81
mohamad-sliman-ws <73951899+mohamad-sliman-ws@users.noreply.github.com>
2021-05-02 16:09:56 +0300

    Delete assets directory

--- assets/js/dashboard/fade-in.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function FadeIn({show, children}) {
-  const className = show ? "fade-enter-active" : "fade-enter"
-
-  return <div className={className}>{children}</div>
-}
-
--- assets/js/embed.host.js
+++
@@ -1,3 +0,0 @@
-import iframeResize from 'iframe-resizer/js/iframeResizer'
-
-iframeResize({heightCalculationMethod: 'taggedElement'}, '[plausible-embed]')
