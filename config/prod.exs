8cc83cc4c77989bcfde2a06df04ac0b88c1a0b50
mohamad-sliman-ws <73951899+mohamad-sliman-ws@users.noreply.github.com>
2021-05-02 16:21:52 +0300

    Delete config directory

--- config/prod.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :plausible, PlausibleWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: false,
-  server: true,
-  code_reloader: false
